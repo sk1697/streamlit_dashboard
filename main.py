78957252b142a71d6e17da3ddf8d9b4e0241de6b
sk1697 <sk1697@naver.com>
2024-08-06 22:48:17 +0900

    requirements.txt 생성

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
 st.set_page_config(page_title='test dashboard',page_icon='./icon.ico',layout='wide')
 
 st.title('Test Title')
 
 
 st.subheader('✅test sub header')
-st.text('test dataframe')
+# st.text('test dataframe')
 df = pd.read_csv('./2024년도_학년별·학급별 학생수(초)_경상북도교육청.csv')
 
-st.write(df)
+with st.expander('Raw Data'):
+    st.dataframe(df)
